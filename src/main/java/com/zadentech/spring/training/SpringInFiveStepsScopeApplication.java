12c402bd97d542a93642f1d8031c84e76e85e78b
Valentine Nwachukwu <valentine.nwachukwu@zadentech.com>
2020-02-27 12:53:01 -0500

    updating dependencies

--- src/main/java/com/zadentech/spring/training/SpringInFiveStepsScopeApplication.java
+++ src/main/java/com/zadentech/spring/training/SpringInFiveStepsScopeApplication.java
@@ -1,5 +1,7 @@
 package com.zadentech.spring.training;
 
+import com.zadentech.spring.training.scope.PersonDAO;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/zadentech/spring/training/SpringInFiveSteps.java
+++ src/test/java/com/zadentech/spring/training/SpringInFiveSteps.java
@@ -1,4 +1,4 @@
-package com.zadentech.springinfivesteps;
+package com.zadentech.spring.training;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
